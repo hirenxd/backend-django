f6e13218b0ada63f7c5eef3c6d724d31429696b4
Hiren Manoj <hirenz0000@gmail.com>
2026-01-15 14:39:33 +0530

    final change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "Applying database migrations..."
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-echo "Starting Gunicorn (WSGI)..."
-# Using WSGI avoids the 'missing 1 required positional argument' error
-exec gunicorn diary_project.wsgi:application \
-    --workers 3 \
-    --bind 0.0.0.0:8000
 
+exec gunicorn diary_project.wsgi:application \
+  --bind 0.0.0.0:8000
